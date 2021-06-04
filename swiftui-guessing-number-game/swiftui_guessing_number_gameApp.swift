94588ac3c4fdcaa312260752d2ffadc5dd098522
Han-Saem Park <samiparken@gmail.com>
2021-06-04 17:03:57 +0200

    Initial Commit

---
+++ swiftui-guessing-number-game/swiftui_guessing_number_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_guessing_number_gameApp.swift
+//  swiftui-guessing-number-game
+//
+//  Created by Sam on 2021-06-04.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_guessing_number_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
